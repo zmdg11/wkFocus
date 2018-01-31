d8935942c3ef179822ff99980dcf875056d47bda
Michael Garrett <zmdg11@etsu.edu>
2018-01-30 22:42:35 -0500

    Minor fix to data-raw routine for making dataset files. Fixed some inconsistencies in externalpackage usage data$Type was NA, now "focus" dataset vars now in order by 'scope'

--- data-raw/focus_cstamps.R
+++ data-raw/focus_cstamps.R
@@ -10,8 +10,13 @@
 library(devtools)     # use_data
 library(dplyr)        # manipulate dataframes
 library(readxl)       # read excel files
+library(stringr)
 library(purrr)        # map
 library(wkFocus)
+
+## Parameters
+
+pars <- wkFocus::wkf_config()
 
 ## Functions  ----------------------
 
@@ -27,12 +32,12 @@ read_raw_datasets <- function(SID) {
 
   ## Build list of dataset IDs (sid, coder, versrion) from each sheet name
   ds_id <- ds_sheets %>%
-    purrr::map( ~ str_split_fixed(string = ., "_", 2)) %>%
+    purrr::map( ~ stringr::str_split_fixed(string = ., "_", 2)) %>%
     purrr::map( ~ list(sid = SID, coder = .[1], version = .[2]))
 
   ## Read actual data into a list of dfs
   ds_raw <- ds_sheets %>%
-    purrr::map(~ read_excel(path = ds_src, sheet = .))  # list <df, df...>
+    purrr::map(~ readxl::read_excel(path = ds_src, sheet = .))  # list df, df...
 
   ## Fold all the lists together and flip it so each element
   #  will be a fully specified raw dataset
@@ -54,7 +59,7 @@ make_cstamp_datasets <- function(ds_raw) {
   df <- ds_raw$data
   sid <- ds_raw$ds_id$sid
 
-  Type <- "cstamp"
+  Type <- "focus"
 
   # Code from here is specific to research phase data. These session comprises
   # phase (= "res"), round, & group -------------------------
@@ -68,25 +73,32 @@ make_cstamp_datasets <- function(ds_raw) {
   df <- df %>%
     mutate(
       In  = paste("00", In, "00", sep = ":"),
-      Out = paste("00", Out, "00", sep = ":"))
+      Out = paste("00", Out, "00", sep = ":")
+    )
 
   # The video-style timecodes need to be as POSIXct objects with a standard
   # origin for plotting with ggplot2
   df <- df %>%
     mutate(
       In  = wkFocus::wkf_convert_tcode(df$In, pars$fr, pars$t_workshop)$datetime,
-      Out = wkFocus::wkf_convert_tcode(df$Out, pars$fr, pars$t_workshop)$datetime)
+      Out = wkFocus::wkf_convert_tcode(df$Out, pars$fr, pars$t_workshop)$datetime
+    )
 
   # Need standard factors across the project
   df <- df %>%
     mutate(
-      Bin   = factor(Bin, levels = pars$facil_codes, ordered = TRUE),
-      Code  = factor(Code, levels = pars$focus_codes, ordered = TRUE),
       Round = factor(Round, levels = pars$round_levels, ordered = TRUE),
       GID   = factor(GID, levels = pars$GID_levels, ordered = TRUE),
-      Type  = factor(Type, levels = pars$code_types, ordered = TRUE))
+      Type  = factor(Type, levels = pars$code_types, ordered = TRUE),
+      Bin   = factor(Bin, levels = pars$facil_codes, ordered = TRUE),
+      Code  = factor(Code, levels = pars$focus_codes, ordered = TRUE)
+    )
 
-  # Build the cstamp dataset
+  # Put in order by 'scope'
+  df <- df %>%
+    select(Round, GID, Type, Bin, In, Out, Code)
+
+  # Build the cstamp dataset on top of the raw dataset
   ds_raw$ds_type <- "cstamp"
   ds_raw$data <- df
 
