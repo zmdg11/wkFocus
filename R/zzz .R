8e23ad269dce55b288fc0b3338b0f23cf602a1dd
zmdg11 <zmdg11@etsu.edu>
2018-03-06 13:39:30 -0500

    Phase 2 of reconfig: making config file, sysdata, and global options

--- R/zzz .R
+++ R/zzz .R
@@ -13,32 +13,31 @@
   #
   # cf. Wickham, H. [_R packages_, "R Code"][http://r-pkgs.had.co.nz/r.html]
 
-  # Make colors named vectors
-  focus_codes = c("Rd", "Do", "Rp", "Ds", "O", "Of")
-  focus_cols  = c("#8c510a","#d8b365","#FF7F00", "#CD1076","#5ab4ac","#01665e")
-  names(focus_cols) <- focus_codes
+  library(jsonlite)
 
-  facil_codes = c("Ind", "Fac")
-  facil_cols = c('#bdbdbd', '#636363')  # BW
-  # facil_cols = c("#f1a340", "olivedrab3")
-  names(facil_cols) <- facil_codes
+  # Get preload decisions from config file
+  config <- read_json("./data-raw/config.json", simplifyVector = TRUE)
 
-  ## Add wkf options to the global options list
-  op <- options()
-  op.wkf <- list(
-    wkf.code.types = c("facilitation", "focus"),
-    wkf.focus.codes = focus_codes,
-    wkf.focus.cols = focus_cols,
-    wkf.facil.codes = facil_codes,
-    wkf.facil.cols = facil_cols,
-    wkf.time.workshop = paste("2016-08-16", "00:00:00 UTC"),  # conventional origin
-    wkf.pltime.min = paste("2016-08-16", "00:00:00 UTC"),     # def start of plot
-    wkf.pltime.max = paste("2016-08-16", "00:20:00 UTC"),     # def end of plot
-    wkf.fr = 25  # frame rate the video was coded at
+  # Default options use study tables (`study`) from package data, and build
+  #  parameters (`pars`) from package internal data.
+
+  # Make default color lists have the names of the codes they color.
+  names(config$facil_cols) <- wkFocus::study$facilitation$code
+  names(config$focus_cols) <- wkFocus::study$focus$code
+
+  # Default list will be used to set user-configurable global options
+  #  Use "." as semantic delim as Wickham did. something I don't know?
+  defaults <- list(
+    wkf.ses.start = pars$workshop_start,
+    wkf.pl.duration = as.difftime(config$pl_duration, format = "%T"),
+    wkf.focus.cols = config$focus_cols,
+    wkf.facil.cols = config$facil_cols
   )
 
-  toset <- !(names(op.wkf) %in% names(op))   # only those not already defined
-  if(any(toset)) options(op.wkf[toset])
+  op <- options()
+
+  toset <- !(names(defaults) %in% names(op))   # only those not already defined
+  if(any(toset)) options(defaults[toset])
 
   invisible()
 }
