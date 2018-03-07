69e975ba08050c889ba9edaa6ccca93a201eca64
zmdg11 <zmdg11@etsu.edu>
2018-03-07 13:00:09 -0500

    Fix issue with reconfig where.onLoad needing access to the config file.

--- R/zzz .R
+++ R/zzz .R
@@ -1,37 +1,16 @@
-#' .onLoad
-#'
-#' \code{.onLoad} sets user configurable parameters via R global options. R will execute this function, passing the correct arguments, when this package is loaded.
-#'
-#' @param libname
-#' @param pkgname
-#'
-#' @return
-#'
 .onLoad <- function(libname, pkgname) {
 
   ## User configurable options
   #
   # cf. Wickham, H. [_R packages_, "R Code"][http://r-pkgs.had.co.nz/r.html]
 
-  library(jsonlite)
-
-  # Get preload decisions from config file
-  config <- read_json("./data-raw/config.json", simplifyVector = TRUE)
-
-  # Default options use study tables (`study`) from package data, and build
-  #  parameters (`pars`) from package internal data.
-
-  # Make default color lists have the names of the codes they color.
-  names(config$facil_cols) <- wkFocus::study$facilitation$code
-  names(config$focus_cols) <- wkFocus::study$focus$code
-
   # Default list will be used to set user-configurable global options
   #  Use "." as semantic delim as Wickham did. something I don't know?
   defaults <- list(
     wkf.ses.start = pars$workshop_start,
-    wkf.pl.duration = as.difftime(config$pl_duration, format = "%T"),
-    wkf.focus.cols = config$focus_cols,
-    wkf.facil.cols = config$facil_cols
+    wkf.facil.cols = pars$defaults$facil_cols,
+    wkf.focus.cols = pars$defaults$focus_cols,
+    wkf.pl.duration = pars$defaults$pl_duration
   )
 
   op <- options()
--- data-raw/configure-project.R
+++ data-raw/configure-project.R
@@ -1,9 +1,13 @@
 library(jsonlite)
+library(wkFocus)
 
 ## Get preload decisions from config file
 config <- read_json("./data-raw/config.json", simplifyVector = TRUE)
 
 ## Configure: Prebuild decisions ---------------------
+
+names(config$facil_cols) <- wkFocus::study$facilitation$code
+names(config$focus_cols) <- wkFocus::study$focus$code
 
 # Build the params list
 pars <- list(
@@ -11,7 +15,13 @@ pars <- list(
   code_types = config$code_types,
   rounds = config$rounds,
   gids = config$gids,
-  workshop_start = as.POSIXct(config$workshop_start, tz = "UTC")
+  workshop_start = as.POSIXct(config$workshop_start, tz = "UTC"),
+  defaults = list(
+    ses_start = as.POSIXct(config$workshop_start, tz = "UTC"),
+    facil_cols = config$facil_cols,
+    focus_cols = config$focus_cols,
+    pl_duration = as.difftime(config$pl_duration, format = "%T")
+  )
 )
 
 # Save as package internal data
